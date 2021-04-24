be907599bf8cdb5cc439c2e0145dc69705f3461b
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-04-23 23:34:12 -0400

    Dynamically add each astronaut returned to the web page matching HTML template provided

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // TODO: add code here
 window.addEventListener("load", function(){
   fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-    
+    response.json().then(function(json){
+      let astronauts = json;
+      const container = document.getElementById("container");
+      for(let astronaut of astronauts){
+        let skills = astronaut.skills;
+        container.innerHTML += `
+          <div class="astronaut">
+            <div class="bio">
+              <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+              <ul>
+                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                <li>Active: ${astronaut.active}</li>
+                <li>Skills: ${skills.map(skill => `${skill}`).join(", ")}</li>
+              </ul>
+            </div>
+            <img class="avatar" src=${astronaut.picture}>
+          </div>
+        `;
+      }
+    });
   });
 });
