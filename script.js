eb6e99c7a551dc881ff5cd13ce18b463d0ed2e75
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-04-24 00:35:16 -0400

    Bonus Mission: Add a count of astronauts to the page.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener("load", function(){
       let astronauts = json;
       astronauts.sort((a, b) => b.hoursInSpace - a.hoursInSpace);
       const container = document.getElementById("container");
+      container.innerHTML = `<p>Astronaut Count: ${astronauts.length}</p>`;
       for(let astronaut of astronauts){
         let skills = astronaut.skills;
         container.innerHTML += `
@@ -13,7 +14,7 @@ window.addEventListener("load", function(){
               <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
               <ul>
                 <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                <li>Active: ${astronaut.active}</li>
+                <li ${(astronaut.active ? `class="active"` : "")}>Active: ${astronaut.active}</li>
                 <li>Skills: ${skills.map(skill => `${skill}`).join(", ")}</li>
               </ul>
             </div>
